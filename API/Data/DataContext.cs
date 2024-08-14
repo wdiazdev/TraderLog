9b5e6a47318252d57e23684094327fc7ec576dbc
Wilfredo Diaz <wdiazdev@gmail.com>
2024-08-14 16:21:31 -0400

    Migration created

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
