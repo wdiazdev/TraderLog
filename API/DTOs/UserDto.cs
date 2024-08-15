c8c7683f547085261a88aead5e65e84590095e4c
Wilfredo Diaz <wdiazdev@gmail.com>
2024-08-15 19:54:40 -0400

    Jwt service added. UserDto created.

---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,8 @@
+namespace API.DTOs
+{
+    public class UserDto
+    {
+        public int UserId { get; set; }
+        public string Token { get; set; }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,16 @@
 using API.Data;
 using API.Entities;
+using API.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -25,6 +25,7 @@ builder.Services.AddIdentityCore<User>(opt =>
 
 builder.Services.AddAuthentication();
 builder.Services.AddAuthorization();
+builder.Services.AddScoped<TokenService>();
 
 
 var app = builder.Build();
