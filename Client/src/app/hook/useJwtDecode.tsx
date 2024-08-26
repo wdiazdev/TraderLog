d942831dc179af8768aa9c54380252c5f4e216f3
Wilfredo Diaz <wdiazdev@gmail.com>
2024-08-26 05:43:56 -0400

    side bar decode name refactored.

---
+++ Client/src/app/hook/useJwtDecode.tsx
@@ -0,0 +1,22 @@
+import { jwtDecode } from "jwt-decode"
+
+interface JwtPayload {
+  [key: string]: any
+}
+
+export default function useJwtDecode(claimType: "role" | "emailaddress" | "name") {
+  let claimData: string | undefined = undefined
+
+  const token = localStorage.getItem("user")
+  if (token) {
+    try {
+      const decodedToken = jwtDecode<JwtPayload>(token)
+
+      claimData = decodedToken[`http://schemas.xmlsoap.org/ws/2005/05/identity/claims/${claimType}`]
+    } catch (error) {
+      console.error("Failed to decode token:", error)
+    }
+  }
+
+  return { claimData }
+}
--- Client/src/components/SideBar.tsx
+++ Client/src/components/SideBar.tsx
@@ -4,28 +4,33 @@ import QueryStatsIcon from "@mui/icons-material/QueryStats"
 import { TabOptions } from "../model/dashboard"
 import { chartOneUpperCase } from "../helper/chartOneUpperCase"
 import dayjs from "dayjs"
+import useJwtDecode from "../app/hook/useJwtDecode"
 
 interface Props {
   handleTabChange: (tab: TabOptions) => void
 }
 
+const tabs: TabOptions[] = ["performance", "settings"]
+
 export default function SideBar({ handleTabChange }: Props) {
-  const tabs: TabOptions[] = ["performance", "settings"]
+  const { claimData } = useJwtDecode("name")
 
   return (
     <div className="w-[260px] bg-bkg-2 rounded-lg">
       <div className="flex flex-col items-center">
         <h3 className="text-white text-[24px] text-center m-4">{dayjs().format("ddd, DD YYYY")}</h3>
-        <Avatar
-          sx={{
-            bgcolor: "rgb(39,194,232)",
-            fontSize: 32,
-            width: 56,
-            height: 56,
-          }}
-        >
-          W
-        </Avatar>
+        {claimData && (
+          <Avatar
+            sx={{
+              bgcolor: "rgb(39,194,232)",
+              fontSize: 32,
+              width: 56,
+              height: 56,
+            }}
+          >
+            {claimData.charAt(0).toUpperCase()}
+          </Avatar>
+        )}
       </div>
 
       <div className="mt-12">
