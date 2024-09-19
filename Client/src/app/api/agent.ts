d1388b7a48a309c95ebc7f74cf35d47c4becbe3f
unknown <wdiazdev@gmail.com>
2024-09-19 11:51:27 -0400

    user account fetch added to agent

--- Client/src/app/api/agent.ts
+++ Client/src/app/api/agent.ts
@@ -1,65 +1,76 @@
-import axios, { AxiosError, AxiosResponse } from "axios"
-import { toast } from "react-toastify"
-import { store } from "../store/configureStore"
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { toast } from "react-toastify";
+import { store } from "../store/configureStore";
 
-const sleep = () => new Promise((resolve) => setTimeout(resolve, 500))
+const sleep = () => new Promise((resolve) => setTimeout(resolve, 500));
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
-const responseBody = (response: AxiosResponse) => response.data
+const responseBody = (response: AxiosResponse) => response.data;
 
 axios.interceptors.request.use((config) => {
-  const token = store.getState().account.user?.token
-  if (token) config.headers.Authorization = `Bearer ${token}`
-  return config
-})
+  const token = store.getState().account.user?.token;
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
 axios.interceptors.response.use(
   async function (response) {
-    if (import.meta.env.DEV) await sleep()
-    return response
+    if (import.meta.env.DEV) await sleep();
+    return response;
   },
   function (error: AxiosError) {
     if (error) {
-      const { data, status } = error.response as AxiosResponse
+      const { data, status } = error.response as AxiosResponse;
       switch (status) {
         case 400:
           if (data && data.errors) {
-            const modelStateErrors: string[] = []
+            const modelStateErrors: string[] = [];
             for (const key in data.errors) {
-              modelStateErrors.push(data.errors[key])
+              modelStateErrors.push(data.errors[key]);
             }
-            throw modelStateErrors.flat()
+            throw modelStateErrors.flat();
           }
-          toast.error(data.title)
-          break
+          toast.error(data.title);
+          break;
         case 401:
-          toast.error(data.title)
-          break
+          toast.error(data.title);
+          break;
         case 500:
           //   router.navigate("/server-error", { state: { error: data } })
-          break
+          break;
         default:
-          toast.error("An unexpected error occurred")
-          break
+          toast.error("An unexpected error occurred");
+          break;
       }
-      return Promise.reject(error.response)
+      return Promise.reject(error.response);
     }
-  },
-)
+  }
+);
 
 const requests = {
   get: (url: string) => axios.get(url).then(responseBody),
   post: (url: string, body: object) => axios.post(url, body).then(responseBody),
   put: (url: string, body: object) => axios.put(url, body).then(responseBody),
   delete: (url: string) => axios.delete(url).then(responseBody),
-}
+};
 
 const Account = {
   login: (values: any) => requests.post("account/login", values),
   register: (values: any) => requests.post("account/register", values),
   currentUser: () => requests.get("account/currentUser"),
-}
+};
+
+const UserAccounts = {
+  createAccount: (values: any) =>
+    requests.post("userAccounts/createAccount", values),
+  getAllAccounts: () => requests.get("userAccounts"),
+  getAccountById: (id: number) => requests.get(`userAccounts/${id}`),
+  deleteAccount: (id: number) =>
+    requests.delete(`userAccounts/deleteAccount/${id}`),
+  updateAccount: (values: any) =>
+    requests.put("userAccounts/updateAccount", values),
+};
 
 const TestErrors = {
   get400Error: () => requests.get("buggy/bad-request"),
@@ -67,11 +78,12 @@ const TestErrors = {
   get404Error: () => requests.get("buggy/not-found"),
   get500Error: () => requests.get("buggy/server-error"),
   getValidationError: () => requests.get("buggy/validation-error"),
-}
+};
 
 const agent = {
   Account,
+  UserAccounts,
   TestErrors,
-}
+};
 
-export default agent
+export default agent;
